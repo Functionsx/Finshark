bb19962c24183f4ddf8233627f937e27e2f664fc
Salma B <inverse@tuta.io>
2025-04-11 12:36:01 +0100

    Commiting Finshark app from my local VS Code to Github

---
+++ api/Program.cs
@@ -0,0 +1,33 @@
+using api.Data;
+using api.Interfaces;
+using api.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IStockRepository, StockRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
